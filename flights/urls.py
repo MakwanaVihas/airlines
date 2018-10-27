7cd4d1096209ea0e33bd8a662b52799d1d7fdde7
MakwanaVias <makvihas@gmail.com>
2018-10-28 03:20:33 +0530

    First pages commit

--- flights/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-path("",views.index,name='index'),
-path("<int:fid>",views.flight,name='flight'),
-path("<int:fid>/book",views.book,name="book"),
-]
